3c2deaf035a88da4c29d7f93e89b0c9fed9cee56
felix mavila <felixmacarenco@gmail.com>
2024-02-21 15:19:21 +0000

    chore: create signp page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,46 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image'
+import { Button } from '@/components/ui/Button'
 import { Input } from '@/components/ui/Input'
 
+import Link from 'next/link'
 import logoImg from '../../public/logo.svg'
 export default function Home() {
   return (
     <>
     <Head>
-      <title>ArquivoGPT - Faca seu login</title>
+      <title>ArquivoGPT - Faça seu login</title> 
     </Head>
-
     <div className={styles.containerCenter}>
-      <Image src={logoImg} alt='Logo ArquivoGPT'/>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
+
       <div className={styles.login}>
         <form>
-        <Input 
-        placeholder='Digite o seu email'
-        type="text"
-        />
-        <Input
-        placeholder='Digite a sua Senha'
-        type="password"
-        />
-        </form>               
-      </div> 
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+
+          <Input
+            placeholder="Sua senha"
+            type="password"
+          />
+          
+          <Button
+            type="submit"
+            loading={false}
+          >
+            Acessar
+          </Button>
+        </form>
+
+          <Link href='/signup'>
+          <span className={styles.text}>Nao possui uma conta? Cadastre-se</span>
+          </Link>
+        
+
+      </div>
     </div>
     </>
   )
